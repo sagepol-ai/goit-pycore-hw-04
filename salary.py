04ec59dd066c65a8c0b742b46ba3fc5c9342a1c0
Oleksandr Poletaiev <sagepol@gmail.com>
2025-10-19 16:11:15 +0200

    DZ4

---
+++ salary.py
@@ -0,0 +1,12 @@
+salary_file = 'salaries.txt'
+def total_salary(salary_file):
+   
+    with open(salary_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()  # читаємо всі рядки одразу
+
+    salaries = [int(line.strip().split(',')[1]) for line in lines]
+    total = sum(salaries)
+    average = total / len(salaries)
+    return total, average
+total, average = total_salary(salary_file)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")  
